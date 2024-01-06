b3c6935fb46f1b40a3af2138316cc7bad78ced7b
ivy <ivy16203@gmail.com>
2024-01-06 11:42:09 +0100

    add basic nav styling

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ enableTooltips()
 
 // tiny SPA router
 {
+    const navlinks = document.querySelectorAll('nav a')
+
     document.body.classList.add('js-enabled')
 
     let curpage: HTMLElement | undefined
+
     function loadPage(url) {
         const pageName = url.split('#')[1] ?? 'intro'
         const page = document.querySelector(`[data-page="${pageName}"]`) as HTMLElement
@@ -19,6 +22,15 @@ enableTooltips()
         curpage = page
         curpage.style.display = 'block'
         window.scrollTo(0, 0);
+
+        for (let link of navlinks) {
+            const href = link.href.split('#')[1]
+            if (href == pageName) {
+                link.classList.add('selected')
+            } else {
+                link.classList.remove('selected')
+            }
+        }
     }
 
     loadPage(window.location.href)
